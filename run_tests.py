0f27448b150dbe21707c8ed29e66a80be6f70b4e
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-12 14:26:06 +0300

    Add an option to run two program on the same set of tests

--- run_tests.py
+++ run_tests.py
@@ -2,9 +2,22 @@ from os import system
 from sys import argv
 from glob import glob
 
-for test_file in argv[2:]:
+programs = [argv[1], ]
+
+filenames_start = 2
+
+if argv[2] in ['-a', '--against']:
+	programs.append(argv[3])
+
+	filenames_start += 2
+
+for test_file in argv[filenames_start:]:
 	print test_file, ": "
 
-	system("%s < %s" % (argv[1], test_file))
+	for program in programs:
+		if len(programs) > 1:
+			print "\t", program
+
+		system("%s < %s" % (program, test_file))
 
 	print
