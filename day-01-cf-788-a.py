aa793bac50cb0d722fa2d2654953919f9f9a5332
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-12 11:31:26 +0300

    Implement naive solution in O(n^2)

--- day-01-cf-788-a.py
+++ day-01-cf-788-a.py
@@ -4,3 +4,20 @@ n = int(raw_input())
 xs = map(int, raw_input().split(' '))
 
 fs = map(lambda (x, y): abs(x - y), zip(xs, xs[1:]))
+
+max_sum = 0
+
+for l in xrange(n):
+	for r in xrange(l, n):
+		current_sum = 0
+
+		for i in xrange(l, r):
+			if (i - l) % 2 == 0:
+				current_sum += fs[i]
+			else:
+				current_sum -= fs[i]
+
+		if current_sum > max_sum:
+			max_sum = current_sum
+
+print max_sum
