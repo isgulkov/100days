b78c797d2b7db4aa3c8db1feb33d484a43032f9b
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-14 13:48:12 +0300

    Implement naive O(n^2) solution

--- day-03-cf-778-a.c
+++ day-03-cf-778-a.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Returns whether string `p` is a subsequence of string `t` with characters skipped at positions for which
+ * corresponding booleans in `t_removed_positions` are set
+ */
+int is_a_subsequence(char* p, char* t, int* t_removed_positions)
+{
+    int pos_p = 0, pos_t = 0;
+
+    while(p[pos_p] != '\0' && t[pos_t] != '\0') {
+        if(!t_removed_positions[pos_t] && p[pos_p] == t[pos_t]) {
+            pos_p += 1;
+        }
+
+        pos_t += 1;
+    }
+
+    return p[pos_p] == '\0';
+}
+
 int main()
 {
     char t[200001], p[200001];
@@ -11,10 +30,29 @@ int main()
     size_t t_length = strlen(t);
     size_t p_length = strlen(p);
 
-    int is[200000];
+    int t_removed_positions[200001];
 
     for(int i = 0; i < t_length; i++) {
-        scanf("%d", &is[i]);
+        t_removed_positions[i] = 0;
+    }
+
+    for(int i = 0; i < t_length; i++) {
+        int pos;
+
+        scanf("%d", &pos);
+
+        t_removed_positions[pos - 1] = 1;
+
+        if(!is_a_subsequence(p, t, t_removed_positions)) {
+            printf("%d", i);
+            break;
+        }
+    }
+
+    for(int i = 0; i < t_length; i++) {
+        int pos;
+
+        scanf("%d", &pos);
     }
 
     return 0;
