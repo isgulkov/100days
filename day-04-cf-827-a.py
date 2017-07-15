edbd2cf9624a982ef12be3df35cac928f9d07006
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-15 23:23:46 +0300

    Determine the length of the resulting string

--- day-04-cf-827-a.py
+++ day-04-cf-827-a.py
@@ -5,11 +5,22 @@ n = int(raw_input())
 substrings = []
 substring_occurences = {}
 
+s_length = 0
+
 for i in xrange(n):
 	line = raw_input().split(' ')
 
-	substrings.append(line[0])
-	substring_occurences[line[0]] = map(int, line[2:])
+	current_substring = line[0]
+	current_occurences = map(int, line[2:])
+
+	candidate_length = len(current_substring) + current_occurences[-1] - 1
+
+	if candidate_length > s_length:
+		s_length = candidate_length
+
+	substrings.append(current_substring)
+	substring_occurences[line[0]] = current_occurences
 
 print substrings
 print substring_occurences
+print s_length
