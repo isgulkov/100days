200c8465a73e1a055a1acb6d05efb6de0b3e792a
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-15 16:17:15 +0300

    Put the main class in the beginning of the file

--- day-03a-cf-778-b.java
+++ day-03a-cf-778-b.java
@@ -2,6 +2,48 @@ import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.regex.*;
 
+class Day03a
+{
+    private static Scanner stdin = new Scanner(System.in);
+
+    private static int[] read2Ints()
+    {
+        Pattern p = Pattern.compile("(\\d+) (\\d+)");
+        Matcher matcher = p.matcher(stdin.nextLine());
+
+        if(!matcher.find()) {
+            throw new IllegalArgumentException();
+        }
+
+        int a = Integer.parseInt(matcher.group(1));
+        int b = Integer.parseInt(matcher.group(2));
+
+        return new int[] { a, b };
+    }
+
+    public static void main(String[] args)
+    {
+        int n, m;
+
+        {
+            int[] ab = read2Ints();
+            n = ab[0];
+            m = ab[1];
+        }
+
+        VariableSystem vs = new VariableSystem(m);
+
+        for(int i = 0; i < n; i++) {
+            vs.addStatement(stdin.nextLine());
+        }
+
+        VariableSystem.MinMax minMax = vs.getMinMaxArguments();
+
+        System.out.println(minMax.getMin());
+        System.out.println(minMax.getMax());
+    }
+}
+
 /**
  * Represents a bitwise function of Petya's answer
  */
@@ -278,47 +320,5 @@ class VariableSystem
         {
             return max;
         }
-    }
-}
-
-class Day03a
-{
-    private static Scanner stdin = new Scanner(System.in);
-
-    private static int[] read2Ints()
-    {
-        Pattern p = Pattern.compile("(\\d+) (\\d+)");
-        Matcher matcher = p.matcher(stdin.nextLine());
-
-        if(!matcher.find()) {
-            throw new IllegalArgumentException();
-        }
-
-        int a = Integer.parseInt(matcher.group(1));
-        int b = Integer.parseInt(matcher.group(2));
-
-        return new int[] { a, b };
-    }
-
-    public static void main(String[] args)
-    {
-        int n, m;
-
-        {
-            int[] ab = read2Ints();
-            n = ab[0];
-            m = ab[1];
-        }
-
-        VariableSystem vs = new VariableSystem(m);
-
-        for(int i = 0; i < n; i++) {
-            vs.addStatement(stdin.nextLine());
-        }
-
-        VariableSystem.MinMax minMax = vs.getMinMaxArguments();
-
-        System.out.println(minMax.getMin());
-        System.out.println(minMax.getMax());
     }
 }
