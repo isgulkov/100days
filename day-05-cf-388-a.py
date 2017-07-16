02d12777608ec684f8e09633d974177c478c2c0c
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-16 13:37:03 +0300

    Implement naive algorithm

--- day-05-cf-388-a.py
+++ day-05-cf-388-a.py
@@ -1,6 +1,24 @@
 
 raw_input()
 
-xs = map(int, raw_input().split(' '))
+box_caps = map(int, raw_input().split(' '))
 
-print xs
+box_caps.sort()
+
+num_stacks = 0
+
+while len(box_caps) != 0:
+	current_cap = box_caps.pop()
+
+	while current_cap > 0:
+		next_cap = box_caps.pop()
+
+		current_cap -= 1
+
+		if next_cap < current_cap:
+			current_cap = next_cap
+
+	num_stacks += 1
+
+print num_stacks
+
