e925883c2c5643d1745bfe0b32ca02c4c1cb465b
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-16 13:42:16 +0300

    Skip consequtive boxes of the same cap (?)

--- day-05-cf-388-a.py
+++ day-05-cf-388-a.py
@@ -10,13 +10,23 @@ num_stacks = 0
 while len(box_caps) != 0:
 	current_cap = box_caps.pop()
 
-	while current_cap > 0:
-		next_cap = box_caps.pop()
+	i = len(box_caps) - 1
+
+	while i >= 0:
+		if box_caps[i] >= current_cap:
+			i -= 1
+
+			continue
+
+		next_cap = box_caps[i]
+		del box_caps[i]
 
 		current_cap -= 1
 
 		if next_cap < current_cap:
 			current_cap = next_cap
+
+		i -= 1
 
 	num_stacks += 1
 
