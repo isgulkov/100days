fd2500f225484a105ccf1ccc3a5a4f0a1cbdf899
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-16 17:12:23 +0300

    Build the decimal layered graph structure

--- day-05a-cf-388-b.cs
+++ day-05a-cf-388-b.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 class MainClass
 {
@@ -51,6 +53,87 @@ class MainClass
 			+ 7 + 6 + 5 + 4 + 3 + 2 + 1; // chains from layers to node 1
 
 		Graph g = new Graph(n);
+
+		List<int> freeNodes = Enumerable.Range(2, n - 2).ToList();
+
+		List<List<int>> layers = new List<List<int>>();
+
+		/**
+		 * Create 7 layers of 10 nodes each, where for each i in [0; 6] there are 10^i paths between node 0 and each
+		 * node in ith layer
+		 */
+
+		/**
+		 * Assign nodes to layers
+		 */
+		for(int i = 0; i < 7; i++) {
+			List<int> newLayer = new List<int>();
+
+			for(int j = 0; j < 10; j++) {
+				newLayer.Add(freeNodes[0]);
+				freeNodes.RemoveAt(0);
+			}
+
+			layers.Add(newLayer);
+		}
+
+		/**
+		 * Connect node 0 to the first layer
+		 */
+		foreach(int nodeId in layers[0]) {
+			g.AddEdge(0, nodeId);
+		}
+
+		/**
+		 * Fully interconnect each layer with the previous one
+		 */
+		for(int i = 1; i < 7; i++) {
+			foreach(int nodePreviousLayer in layers[i - 1]) {
+				foreach(int nodeThisLayer in layers[i]) {
+					g.AddEdge(nodePreviousLayer, nodeThisLayer);
+				}
+			}
+		}
+
+		/**
+		 * Create chains of appropriate length for connecting each layer to node 1. This is needed so that paths from
+		 * nodes of each layer to node 1 are equally long
+		 */
+		List<int> chainEnds = new List<int>();
+
+		for(int i = 0; i < 6; i++) {
+			List<int> chain = new List<int>();
+
+			for(int j = 0; j < 6 - i; j++) {
+				chain.Add(freeNodes[0]);
+				freeNodes.RemoveAt(0);
+			}
+
+			/**
+			 * At the other end of each chain there's node 1
+			 */
+			chain.Add(1);
+
+			/**
+			 * Connect the chain
+			 */
+			for(int j = 1; j < chain.Count; j++) {
+				g.AddEdge(chain[j - 1], chain[j]);
+			}
+
+			chainEnds.Add(chain[0]);
+		}
+
+		/**
+		 * Connect the appropriate number of nodes of each layer to node 1 through their appropriate chains
+		 */
+		for(int currentLayer = 0; k != 0; k /= 10, currentLayer++) {
+			int currentDecimalPlace = k % 10;
+
+			for(int i = 0; i < currentDecimalPlace; i++) {
+				g.AddEdge(layers[currentLayer][i], chainEnds[currentLayer]);
+			}
+		}
 
 		Console.WriteLine(n);
 
