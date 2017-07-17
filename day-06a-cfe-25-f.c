a17f5f65d2d02f2f36403401859e73fc97db9689
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-17 19:55:25 +0300

    Implement dp over the answers for prefixes of `s`

--- day-06a-cfe-25-f.c
+++ day-06a-cfe-25-f.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <math.h>
 
 /**
  * @param s String of specified size
@@ -28,7 +29,7 @@ size_t prefix_function_last(char* s, size_t size)
 
     size_t prefix_last = prefix_function[size - 1];
 
-    free(prefix_function);
+//    free(prefix_function);
 
     return prefix_last;
 }
@@ -58,8 +59,39 @@ int main()
 
     size_t s_length = strlen(s);
 
+    size_t* dp = malloc(sizeof(size_t) * s_length);
+    dp[0] = 0;
+
+    /**
+     * dp[i] stores the minimum number of chars required for encoding the [0; i) substring
+     */
     for(size_t i = 1; i <= s_length; i++) {
-        printf("%c %ld %ld %ld\n", s[i - 1], i, prefix_function_last(s, i), min_period(s, i));
+        dp[i] = INT32_MAX;
+
+        for(size_t j = 0; j < i; j++) {
+            /**
+             * Try to write substring [j; i) as its minimum period with the number of repetitions prepended
+             */
+
+            size_t substr_min_period = min_period(s + j, i - j);
+
+            size_t candidate_answer = dp[j];
+
+            /**
+             * Length of the minimum period string
+             */
+            candidate_answer += substr_min_period;
+
+            /**
+             * Number of decimal digits needed to write down the number of repetitions
+             */
+            candidate_answer += (int)log10((i - j) / substr_min_period) + 1;
+
+            if(candidate_answer < dp[i]) {
+                dp[i] = candidate_answer;
+            }
+        }
     }
 
+    printf("%ld\n", dp[s_length]);
 }
