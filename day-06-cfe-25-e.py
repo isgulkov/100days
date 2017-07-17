731dc5afac47729904a578f8d537dbca3a6e585a
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-17 18:03:36 +0300

    Change "outflow" to the more clear "outdegree" in a couple of names

--- day-06-cfe-25-e.py
+++ day-06-cfe-25-e.py
@@ -5,39 +5,39 @@ from bisect import bisect_left
 class DirectedGraph:
     def __init__(self, n):
         self._num_nodes = n
-        self._outflow_rate = [0 for i in xrange(n)]
+        self._outdegree = [0 for i in xrange(n)]
         self._reversed_adj_lists = [[] for i in xrange(n)]
 
     def add_edge(self, u, v):
-        self._outflow_rate[u] += 1
+        self._outdegree[u] += 1
         self._reversed_adj_lists[v].append(u)
 
     def get_topological_labels(self):
         labels = [None for i in xrange(self._num_nodes)]
 
-        zero_outflow_nodes = [u for u in xrange(self._num_nodes) if self._outflow_rate[u] == 0]
+        zero_outdegree_nodes = [u for u in xrange(self._num_nodes) if self._outdegree[u] == 0]
 
         next_label = self._num_nodes
 
         # On each iteration, select a node with zero outdegree with the highest index (the latter is done to ensure
         # lexicographically minimal label permutation)
-        while len(zero_outflow_nodes) != 0:
-            u = zero_outflow_nodes.pop()
+        while len(zero_outdegree_nodes) != 0:
+            u = zero_outdegree_nodes.pop()
 
             labels[u] = next_label
 
             next_label -= 1
 
             for v in self._reversed_adj_lists[u]:
-                self._outflow_rate[v] -= 1
+                self._outdegree[v] -= 1
 
                 # If the node is a new zero-outdegree node, insert it into the array maintaining the sorted order. In
                 # practice this `bisect_left`/`insert()` solution works faster than both `Queue.PriorityQueue` and
                 # `heapq`
-                if self._outflow_rate[v] == 0:
-                    insertion_point = bisect_left(zero_outflow_nodes, v)
+                if self._outdegree[v] == 0:
+                    insertion_point = bisect_left(zero_outdegree_nodes, v)
 
-                    zero_outflow_nodes.insert(insertion_point, v)
+                    zero_outdegree_nodes.insert(insertion_point, v)
 
         return labels
 
