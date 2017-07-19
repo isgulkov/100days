9115ee3b13d7f501f76ea991424b273ad2e22702
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-19 18:27:07 +0300

    Process column moves

--- day-08-cf-815-a.py
+++ day-08-cf-815-a.py
@@ -25,5 +25,30 @@ for i in xrange(num_rows):
 	if surplus != 0:
 		playing_field_rows[i] = map(lambda x: x - surplus, playing_field_rows[i])
 
+def get_column(i_col, rows):
+	return [rows[j][i_col] for j in xrange(num_rows)]
+
+def map_column(f, i_col, rows):
+	mapped_column = map(f, get_column(i_col, rows))
+
+	for i in xrange(num_rows):
+		rows[i][i_col] = mapped_column[i]
+
+col_maxes = []
+
+for column in [get_column(i, playing_field_rows) for i in xrange(num_cols)]:
+	col_maxes.append(max(column))
+
+smallest_col_max = min(col_maxes)
+
+for i in xrange(num_cols):
+	surplus = col_maxes[i] - smallest_col_max
+
+	for j in xrange(surplus):
+		moves.append("col %d" % (i + 1, ))
+
+	if surplus != 0:
+		map_column(lambda x: x - surplus, i, playing_field_rows)
+
 for move in moves:
 	print move
