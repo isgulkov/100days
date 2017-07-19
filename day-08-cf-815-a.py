02e0c9aad9d7449b6a1c5fa8e3c83c48308148b5
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-19 16:38:56 +0300

    Process row moves

--- day-08-cf-815-a.py
+++ day-08-cf-815-a.py
@@ -1,9 +1,29 @@
+from sys import maxint
 
 n, m = map(int, raw_input().split(' '))
 
-playing_field = []
+playing_field_rows = []
 
 for i in xrange(n):
 	playing_field_rows.append(map(int, raw_input().split(' ')))
 
-print playing_field_rows
+moves = []
+
+row_maxes = []
+
+for row in playing_field_rows:
+	row_maxes.append(max(row))
+
+smallest_row_max = min(row_maxes)
+
+for i in xrange(len(playing_field_rows)):
+	surplus = row_maxes[i] - smallest_row_max
+
+	for j in xrange(surplus):
+		moves.append("row %d" % (i + 1, ))
+
+	if surplus != 0:
+		playing_field_rows[i] = map(lambda x: x - surplus, playing_field_rows[i])
+
+for move in moves:
+	print move
