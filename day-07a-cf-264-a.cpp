f2b382ce82f3f31c272f1165f871c13b9847fcfd
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-19 03:25:21 +0300

    Rewrite using custom doubly-linked list

--- day-07a-cf-264-a.cpp
+++ day-07a-cf-264-a.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-#include <map>
+#include <vector>
 #include <string>
+
+struct list_node
+{
+    int value;
+    list_node* prev;
+    list_node* next;
+};
+
+inline void insert_before(list_node* mouth, list_node* foot)
+{
+    foot->prev = mouth->prev;
+    foot->next = mouth;
+
+    if(mouth->prev != nullptr) {
+        mouth->prev->next = foot;
+    }
+
+    mouth->prev = foot;
+}
+
+inline void insert_after(list_node* mouth, list_node* foot)
+{
+    foot->next = mouth->next;
+    foot->prev = mouth;
+
+    if(mouth->next != nullptr) {
+        mouth->next->prev = foot;
+    }
+
+    mouth->next = foot;
+}
 
 int main()
 {
@@ -8,24 +39,40 @@ int main()
 
     std::cin >> s;
 
-    std::map<double, int> masik;
+    std::vector<list_node> nodes(1000000);
 
-    double left = 0.0, right = 1000000000.0;
+    list_node* head = &nodes[0];
 
-    for(int i = 0; i < s.length(); i++) {
-        double mid = (right + left) / 2.0;
+    head->value = 1;
+    head->prev = nullptr;
+    head->next = nullptr;
 
-        masik[mid] = i + 1;
+    list_node* prev_node = head;
+
+    for(int i = 0; i < s.length() - 1; i++) {
+        list_node* new_node = &nodes[i + 1];
+
+        new_node->value = i + 2;
+        new_node->prev = nullptr;
+        new_node->next = nullptr;
 
         if(s[i] == 'l') {
-            right = mid;
+            insert_before(prev_node, new_node);
+
+            if(new_node->prev == nullptr) {
+                head = new_node;
+            }
         }
         else if(s[i] == 'r') {
-            left = mid;
+            insert_after(prev_node, new_node);
         }
+
+        prev_node = new_node;
     }
 
-    for(auto it = masik.begin(); it != masik.end(); it++) {
-        std::cout << it->second << std::endl;
+    while(head != nullptr) {
+        std::cout << head->value << std::endl;
+
+        head = head->next;
     }
 }
