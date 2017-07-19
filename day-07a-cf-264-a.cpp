8beeeb9c63871c48eefbd0df77db361d55ebad53
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-19 03:44:47 +0300

    Rewrite using std::list again

--- day-07a-cf-264-a.cpp
+++ day-07a-cf-264-a.cpp
@@ -1,38 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <list>
 #include <string>
 #include <stdio.h>
-
-struct list_node
-{
-    int value;
-    list_node* prev;
-    list_node* next;
-};
-
-inline void insert_before(list_node* mouth, list_node* foot)
-{
-    foot->prev = mouth->prev;
-    foot->next = mouth;
-
-    if(mouth->prev != nullptr) {
-        mouth->prev->next = foot;
-    }
-
-    mouth->prev = foot;
-}
-
-inline void insert_after(list_node* mouth, list_node* foot)
-{
-    foot->next = mouth->next;
-    foot->prev = mouth;
-
-    if(mouth->next != nullptr) {
-        mouth->next->prev = foot;
-    }
-
-    mouth->next = foot;
-}
 
 int main()
 {
@@ -40,40 +9,22 @@ int main()
 
     std::cin >> s;
 
-    std::vector<list_node> nodes(1000000);
+    std::list<int> stones = { 1 };
 
-    list_node* head = &nodes[0];
-
-    head->value = 1;
-    head->prev = nullptr;
-    head->next = nullptr;
-
-    list_node* prev_node = head;
+    auto last_node = stones.begin();
 
     for(int i = 0; i < s.length() - 1; i++) {
-        list_node* new_node = &nodes[i + 1];
-
-        new_node->value = i + 2;
-        new_node->prev = nullptr;
-        new_node->next = nullptr;
-
-        if(s[i] == 'l') {
-            insert_before(prev_node, new_node);
-
-            if(new_node->prev == nullptr) {
-                head = new_node;
-            }
-        }
-        else if(s[i] == 'r') {
-            insert_after(prev_node, new_node);
+        if(s[i] == 'r') {
+            /**
+             * Increment iterator so that we insert the new node after the last node instead of before it
+             */
+            last_node++;
         }
 
-        prev_node = new_node;
+        last_node = stones.insert(last_node, i + 2);
     }
 
-    while(head != nullptr) {
-        printf("%d\n", head->value);
-
-        head = head->next;
+    for(auto it = stones.begin(); it != stones.end(); it++) {
+        printf("%d\n", *it);
     }
 }
