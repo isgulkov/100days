7050d549a7869fea919b7d2d6314203a931dfbf4
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-19 20:44:22 +0300

    Reindent with spaces

--- day-08-cf-815-a.py
+++ day-08-cf-815-a.py
@@ -5,7 +5,7 @@ num_rows, num_cols = map(int, raw_input().split(' '))
 playing_field_rows = []
 
 for i in xrange(num_rows):
-	playing_field_rows.append(map(int, raw_input().split(' ')))
+    playing_field_rows.append(map(int, raw_input().split(' ')))
 
 moves = []
 
@@ -14,72 +14,72 @@ moves = []
 row_maxes = []
 
 for row in playing_field_rows:
-	row_maxes.append(max(row))
+    row_maxes.append(max(row))
 
 smallest_row_max = min(row_maxes)
 
 for i_row in xrange(num_rows):
-	surplus = row_maxes[i_row] - smallest_row_max
+    surplus = row_maxes[i_row] - smallest_row_max
 
-	for j in xrange(surplus):
-		moves.append("row %d" % (i_row + 1, ))
+    for j in xrange(surplus):
+        moves.append("row %d" % (i_row + 1, ))
 
-	if surplus != 0:
-		playing_field_rows[i_row] = map(lambda x: x - surplus, playing_field_rows[i_row])
+    if surplus != 0:
+        playing_field_rows[i_row] = map(lambda x: x - surplus, playing_field_rows[i_row])
 
 # Remove surpluses from columns
 
 def get_column(i_col, rows):
-	return [rows[j][i_col] for j in xrange(num_rows)]
+    return [rows[j][i_col] for j in xrange(num_rows)]
 
 def map_column(f, i_col, rows):
-	mapped_column = map(f, get_column(i_col, rows))
+    mapped_column = map(f, get_column(i_col, rows))
 
-	for i in xrange(num_rows):
-		rows[i][i_col] = mapped_column[i]
+    for i in xrange(num_rows):
+        rows[i][i_col] = mapped_column[i]
 
 col_maxes = []
 
 for column in [get_column(i, playing_field_rows) for i in xrange(num_cols)]:
-	col_maxes.append(max(column))
+    col_maxes.append(max(column))
 
 smallest_col_max = min(col_maxes)
 
 for i_col in xrange(num_cols):
-	surplus = col_maxes[i_col] - smallest_col_max
+    surplus = col_maxes[i_col] - smallest_col_max
 
-	for j in xrange(surplus):
-		moves.append("col %d" % (i_col + 1, ))
+    for j in xrange(surplus):
+        moves.append("col %d" % (i_col + 1, ))
 
-	if surplus != 0:
-		map_column(lambda x: x - surplus, i_col, playing_field_rows)
+    if surplus != 0:
+        map_column(lambda x: x - surplus, i_col, playing_field_rows)
 
 # At this point all cells must have the same and non-negative value
 
 common_value = playing_field_rows[0][0]
 
 if common_value < 0:
-	print -1
-	exit(0)
+    print -1
+    exit(0)
 
 for i_row in xrange(num_rows):
-	for i_col in xrange(num_cols):
-		current_cell = playing_field_rows[i_row][i_col]
+    for i_col in xrange(num_cols):
+        current_cell = playing_field_rows[i_row][i_col]
 
-		if current_cell != common_value:
-			print -1
-			exit(0)
+        if current_cell != common_value:
+            print -1
+            exit(0)
 
 if num_rows <= num_cols:
-	for i_row in xrange(num_rows):
-		for j in xrange(common_value):
-			moves.append("row %d" % (i_row + 1, ))
+    for i_row in xrange(num_rows):
+        for j in xrange(common_value):
+            moves.append("row %d" % (i_row + 1, ))
 else:
-	for i_col in xrange(num_cols):
-		for j in xrange(common_value):
-			moves.append("col %d" % (i_col + 1, ))
+    for i_col in xrange(num_cols):
+        for j in xrange(common_value):
+            moves.append("col %d" % (i_col + 1, ))
 
 print len(moves)
 
 for move in moves:
-	print move
+    print move
