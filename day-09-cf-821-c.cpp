0945b67b2d806ee9049626d15617ebb151372993
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-20 16:19:33 +0300

    Implement naive solution manually reordering the stack

--- day-09-cf-821-c.cpp
+++ day-09-cf-821-c.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 #include <string>
+#include <queue>
 
 int main()
 {
@@ -8,7 +9,10 @@ int main()
 
     std::cin >> n;
 
-    std::vector<int> stack;
+    std::stack<int> stack;
+
+    int num_reorderings = 0;
+    int next_box_to_remove = 1;
 
     for(int i = 0; i < 2 * n; i++) {
         std::string command;
@@ -20,12 +24,36 @@ int main()
 
             std::cin >> k;
 
-            stack.push_back(k);
+            stack.push(k);
         }
         else if(command == "remove") {
-            stack.pop_back();
+            if(stack.top() != next_box_to_remove) {
+                num_reorderings += 1;
+
+                std::priority_queue<int> queue;
+
+                int current_box;
+
+                do {
+                    current_box = stack.top();
+
+                    stack.pop();
+
+                    queue.push(current_box);
+                } while(!stack.empty());
+
+                while(!queue.empty()) {
+                    stack.push(queue.top());
+
+                    queue.pop();
+                }
+            }
+
+            stack.pop();
+
+            next_box_to_remove += 1;
         }
     }
 
-    std::cout << stack.size();
+    std::cout << num_reorderings << std::endl;
 }
