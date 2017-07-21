4e03533d8ec60301988b2648edc7a4e995729dd9
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-22 01:20:02 +0300

    Implement SOME solution

--- day-10-cf-424-a.cpp
+++ day-10-cf-424-a.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
 int main()
 {
@@ -17,15 +19,41 @@ int main()
         people.push_back(person);
     }
 
-    std::vector<int> keys;
+    std::set<int> keys;
 
     for(int i = 0; i < num_keys; i++) {
         int key;
 
         std::cin >> key;
 
-        keys.push_back(key);
+        keys.insert(key);
     }
 
-    
+    std::sort(people.begin(), people.end());
+
+    int max_distance = 0;
+
+    for(int i = 0; i < num_people; i++) {
+        int person = people[i];
+
+        int person_distance = INT32_MAX;
+        int selected_key;
+
+        for(int key : keys) {
+            int current_distance = abs(person - key) + abs(key - p);
+
+            if(current_distance < person_distance) {
+                person_distance = current_distance;
+                selected_key = key;
+            }
+        }
+
+        keys.erase(selected_key);
+
+        if(person_distance > max_distance) {
+            max_distance = person_distance;
+        }
+    }
+
+    std::cout << max_distance << std::endl;
 }
