6b865ca7bd00e1bbd10f86da7bb3ba5150f6784a
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-22 00:19:07 +0300

    Use min/max bounds instead of set traversal to check subarray validity

--- day-09a-cf-811-c.cs
+++ day-09a-cf-811-c.cs
@@ -19,42 +19,16 @@ class MainClass
 		 * Determine the smallest intervals (borders inclusive) in which passengers of each city occur (from the first
 		 * occurence of a passenger to the city to the last)
 		 */
-		int[] left_boundary = new int[5001];
+		int[] leftBoundary = new int[5001];
 
 		for(int i = passengerCities.Length - 1; i >= 0; i--) {
-			left_boundary[passengerCities[i]] = i;
+			leftBoundary[passengerCities[i]] = i;
 		}
 
-		int[] right_boundary = new int[5001];
+		int[] rightBoundary = new int[5001];
 
-		for(int i = 0; i < n; i++) {
-			right_boundary[passengerCities[i]] = i;
-		}
-
-		int[,] carriageComfort = new int[passengerCities.Length, passengerCities.Length];
-
-		for(int i = 0; i < n; i++) {
-			if(left_boundary[passengerCities[i]] < i) {
-				continue;
-			}
-
-			HashSet<int> keys = new HashSet<int>();
-			int currentComfort = 0;
-
-			for(int j = i; j < n; j++) {
-				int rightKey = passengerCities[j];
-
-				if(j < right_boundary[rightKey]) {
-					continue;
-				}
-
-				if(!keys.Contains(rightKey)) {
-					keys.Add(rightKey);
-					currentComfort ^= rightKey;
-				}
-
-				carriageComfort[i, j] = currentComfort;
-			}
+		for(int i = 0; i < passengerCities.Length; i++) {
+			rightBoundary[passengerCities[i]] = i;
 		}
 
 		/**
@@ -65,11 +39,42 @@ class MainClass
 		for(int i = 0; i < n; i++) {
 			maxComfort[i + 1] = maxComfort[i];
 
-			for(int j = i; j >= 0; j--) {
-				int candidateMax = maxComfort[j] + carriageComfort[j, i];
+			HashSet<int> subarrayCities = new HashSet<int>();
 
-				if(candidateMax > maxComfort[i + 1]) {
-					maxComfort[i + 1] = candidateMax;
+			int minLeft = int.MaxValue, maxRight = int.MinValue;
+			int subarrayComfort = 0;
+
+			for(int j = i; j >= 0; j--) {
+				/**
+				 * If [j; i] is a valid carriage, try to update the answer for [0; i] with existing sum of best set of
+				 * carriages on [0; j) with it extended by the [j; i] carriage
+				 */
+				int currentCity = passengerCities[j];
+
+				if(!subarrayCities.Contains(currentCity)) {
+					subarrayCities.Add(currentCity);
+					subarrayComfort ^= currentCity;
+
+					/**
+					 * Extend the boundaries the current subarray must contain to be a valid carriage
+					 */
+					if(leftBoundary[currentCity] < minLeft) {
+						minLeft = leftBoundary[currentCity];
+					}
+
+					if(rightBoundary[currentCity] > maxRight) {
+						maxRight = rightBoundary[currentCity];
+					}
+				}
+
+				if(minLeft < j || i < maxRight) {
+					continue;
+				}
+
+				int candidate_max = maxComfort[j] + subarrayComfort;
+
+				if(candidate_max > maxComfort[i + 1]) {
+					maxComfort[i + 1] = candidate_max;
 				}
 			}
 		}
