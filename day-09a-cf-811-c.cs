586cdf37e38da67b6cbfe9ac616a5fea8a8cd0bf
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-21 20:07:20 +0300

    Implement dp algorithm

--- day-09a-cf-811-c.cs
+++ day-09a-cf-811-c.cs
@@ -16,17 +16,81 @@ class MainClass
 		List<int> passengerCities = ReadNInts(n);
 
 		/**
-		 * Determine the smallest intervals (borders inclusive) in which passengers of each city lay (from the first
+		 * Determine the smallest intervals (borders inclusive) in which passengers of each city occur (from the first
 		 * occurence of a passenger to the city to the last)
-		 *
-		 * By formulation, if a passenger from a city is included, all passengers from this city should be included and
-		 * in the same non-overlapping inter, thus including a city means including at least its corresponding
-		 * interval
 		 */
 		Dictionary<int, Tuple<int, int>> cityIntervals = GetOccurenceIntervals(passengerCities);
 
-		foreach(int k in cityIntervals.Keys) {
-			Console.WriteLine($"{k}: {cityIntervals[k]}");
+		CarriageValidityChecker checker = new CarriageValidityChecker(passengerCities, cityIntervals);
+
+		/**
+         * maxComfort[i] stores the answer for subproblem for indices [0; i)
+		 */
+		int[] maxComfort = Enumerable.Repeat(0, n + 1).ToArray();
+
+		for(int i = 0; i < n; i++) {
+			HashSet<int> keys = new HashSet<int>();
+			int subarrayComfort = 0;
+
+			for(int j = i; j >= 0; j--) {
+				/**
+				 * If [j; i] is a valid carriage, try to update the answer for [0; i] with existing sum of best set of
+				 * carriages on [0; j) with it extended by the [j; i] carriage
+				 */
+
+				if(!keys.Contains(passengerCities[j])) {
+					subarrayComfort ^= passengerCities[j];
+
+					keys.Add(passengerCities[j]);
+				}
+
+				if(checker.IsValidCarriage(j, i, keys)) {
+					int candidate_max = maxComfort[j] + subarrayComfort;
+
+					if(candidate_max > maxComfort[i + 1]) {
+						maxComfort[i + 1] = candidate_max;
+					}
+				}
+			}
+
+			/**
+			 * If the best answer without using the `i`th passenger is better than with it, use the the former instead
+			 */
+			if(maxComfort[i + 1] < maxComfort[i]) {
+				maxComfort[i + 1] = maxComfort[i];
+			}
+		}
+
+		Console.WriteLine(maxComfort[n]);
+	}
+
+	class CarriageValidityChecker
+	{
+		List<int> Passengers;
+		Dictionary<int, Tuple<int, int>> Intervals;
+
+		public CarriageValidityChecker(List<int> passengers, Dictionary<int, Tuple<int, int>> intervals)
+		{
+			Passengers = passengers;
+			Intervals = intervals;
+		}
+
+		/// <returns><c>true</c>, if all occurences of every key that occurs in [start; end] are contained in that
+		/// interval, <c>false</c> otherwise</returns>
+		/// <param name="start">Start</param>
+		/// <param name="end">End</param>
+		/// <param name="keys">Set of keys contained in [start; end]</param>
+		public bool IsValidCarriage(int start, int end, HashSet<int> keys)
+		{
+			foreach(int key in keys) {
+				Tuple<int, int> interval = Intervals[key];
+
+				if(interval.Item1 < start || interval.Item2 > end) {
+					return false;
+				}
+			}
+
+			return true;
 		}
 	}
 
