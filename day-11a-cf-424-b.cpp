dc8203ed1af7acb50c4c119ad9d60d6cc969c63d
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-22 16:15:20 +0300

    Collect set of card values and store first and last occurence for each

--- day-11a-cf-424-b.cpp
+++ day-11a-cf-424-b.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <set>
+#include <unordered_map>
 
 int main()
 {
@@ -18,7 +20,24 @@ int main()
         cards.push_back(card);
     }
 
+    std::set<int> card_values;
+
+    std::unordered_map<int, int> card_first_occurence;
+    std::unordered_map<int, int> card_last_occurence;
+
     for(int i = 0; i < num_cards; i++) {
-        std::cout << cards[i] << " ";
+        int card_value = cards[i];
+
+        card_values.insert(card_value);
+
+        if(card_first_occurence.find(card_value) == card_first_occurence.end()) {
+            card_first_occurence[card_value] = i;
+        }
+
+        card_last_occurence[card_value] = i;
+    }
+
+    for(int cv : card_values) {
+        std::cout << cv << " ";
     }
 }
