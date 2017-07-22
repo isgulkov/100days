bf4a948af8bbb2bc5361832eb142a462284dff51
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-22 23:32:03 +0300

    Use structs instead of tuples for trips

--- day-11-cf-822-c.cpp
+++ day-11-cf-822-c.cpp
@@ -3,20 +3,30 @@
 #include <vector>
 #include <tuple>
 
+struct trip
+{
+    int start;
+    int end;
+    int cost;
+
+public:
+    trip(int start, int end, int cost) : start(start), end(end), cost(cost) { }
+};
+
 int main()
 {
     int num_trips, target_duration;
 
     std::cin >> num_trips >> target_duration;
 
-    std::vector<std::tuple<int, int, int>> trips;
+    std::vector<trip> trips;
 
     for(int i = 0; i < num_trips; i++) {
         int start, end, cost;
 
         scanf("%d %d %d", &start, &end, &cost);
 
-        trips.push_back(std::make_tuple(start, end, cost));
+        trips.push_back(trip(start, end, cost));
     }
 
 
