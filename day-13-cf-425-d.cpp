17e34b5e22fc43def22fb96c944317fe79f08b12
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-25 04:13:07 +0300

    Record tree visit order

--- day-13-cf-425-d.cpp
+++ day-13-cf-425-d.cpp
@@ -4,12 +4,15 @@
 
 class lca_tree
 {
-    int num_nodes;
+    size_t num_nodes;
 
     std::vector<std::vector<int>> children;
 
+    std::vector<int> t_in;
+    std::vector<int> height;
+
 public:
-    lca_tree(int num_nodes) : num_nodes(num_nodes), children((size_t)num_nodes) { }
+    lca_tree(size_t num_nodes) : num_nodes(num_nodes), children(num_nodes), t_in(num_nodes), height(num_nodes) { }
 
     void pidor()
     {
@@ -31,9 +34,18 @@ public:
     }
 
     /**
-     * "Hang" the tree by vertex `u` (so that all vectices become unidirectional and all nodes are reachable from `u`)
+     * "Hang" the tree by vertex `u` (so that all vectices become unidirectional and all nodes are reachable from `u`),
+     * and also preprocessed the tree for least-common-ancestor requests
      * @param u The new root of the tree
      */
+    void hang_by_and_preprocess_lca(int u)
+    {
+        hang_by(u);
+
+        preprocess_lca(u);
+    }
+
+private:
     void hang_by(int u)
     {
         for(int v : children[u]) {
@@ -43,6 +55,31 @@ public:
         }
     }
 
+    void preprocess_lca(int root)
+    {
+        std::vector<int> visit_order;
+
+        record_visit_order(root, visit_order);
+
+        std::cout << "Visit order: ";
+
+        for(int u : visit_order) {
+            std::cout << u << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    void record_visit_order(int u, std::vector<int>& visit_order)
+    {
+        visit_order.push_back(u);
+
+        for(int v : children[u]) {
+            record_visit_order(v, visit_order);
+        }
+    }
+
+public:
     int common_edges(int u, int v, int w)
     {
         return 0;
@@ -65,7 +102,7 @@ int main()
         t.add_edge(u, v - 1);
     }
 
-    t.hang_by(0);
+    t.hang_by_and_preprocess_lca(0);
 
     for(int i = 0; i < num_requests; i++) {
         int u, v, w;
