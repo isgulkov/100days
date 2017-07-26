a02e30f22bd420bf81fa99ea67c09985a1263156
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-26 04:52:23 +0300

    Open up modulo operations

--- day-12a-cf-819-b.py
+++ day-12a-cf-819-b.py
@@ -6,26 +6,26 @@ ps = map(int, raw_input().split(' '))
 deviations = [0 for i in xrange(n)]
 
 for i in xrange(n):
-    # Add abs(ps[i] - ((i + shift) % n + 1)) to each deviations[shift] with absolutes opened up
+    # Add abs(ps[i] - ((i + shift) % n + 1)) to each deviations[shift] with absolutes and modulos opened up
 
     if ps[i] >= i + 1:
         for shift in xrange(0, ps[i] - i):
-            deviations[shift] += ps[i] - ((i + shift) % n + 1)
+            deviations[shift] += ps[i] - (i + shift + 1)
 
         for shift in xrange(ps[i] - i, n - i):
-            deviations[shift] -= ps[i] - ((i + shift) % n + 1)
+            deviations[shift] -= ps[i] - (i + shift + 1)
 
         for shift in xrange(n - i, n):
-            deviations[shift] += ps[i] - ((i + shift) % n + 1)
+            deviations[shift] += ps[i] - (i + shift - n + 1)
     else:
         for shift in xrange(0, n - i):
-            deviations[shift] -= ps[i] - ((i + shift) % n + 1)
+            deviations[shift] -= ps[i] - (i + shift + 1)
 
         for shift in xrange(n - i, n - i + ps[i]):
-            deviations[shift] += ps[i] - ((i + shift) % n + 1)
+            deviations[shift] += ps[i] - (i + shift - n + 1)
 
         for shift in xrange(n - i + ps[i], n):
-            deviations[shift] -= ps[i] - ((i + shift) % n + 1)
+            deviations[shift] -= ps[i] - (i + shift - n + 1)
 
 min_deviation, min_shift = deviations[0], 0
 
