1394090bab2f6ca38fd218a88d5568d839f818c7
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-26 04:03:33 +0300

    Switch loop order (so outer loop goes over p elements)

--- day-12a-cf-819-b.py
+++ day-12a-cf-819-b.py
@@ -5,9 +5,9 @@ ps = map(int, raw_input().split(' '))
 
 deviations = [0 for i in xrange(n)]
 
-for shift in xrange(n):
-    for i_p in xrange(n):
-        deviations[shift] += abs(ps[(i_p - shift) % n] - (i_p + 1))
+for i_p in xrange(n):
+    for shift in xrange(n):
+        deviations[shift] += abs(ps[i_p] - ((i_p + shift) % n + 1))
 
 min_deviation, min_shift = deviations[0], 0
 
