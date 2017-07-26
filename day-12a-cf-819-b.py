8d45856351ef0ef84caa05d0734927007ba6e693
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-26 05:27:04 +0300

    Implement segment with "add progression" and "find min" in O(n)

--- day-12a-cf-819-b.py
+++ day-12a-cf-819-b.py
@@ -1,37 +1,60 @@
 
+class OfflineSegment:
+    def __init__(self, length):
+        self.length = length
+
+        self.values = [0 for i in xrange(length)]
+
+    '''
+    Add members of arithmetic progression with initial term -`first` and difference -1 to indices [l; r)
+    '''
+    def subtract_increasing(self, l, r, first):
+        for i in xrange(l, r):
+            self.values[i] -= first - (i - l)
+
+    '''
+    Add members of arithmetic progression with initial term `first` and difference -1 to indices [l; r)
+    '''
+    def add_decreasing(self, l, r, first):
+        for i in xrange(l, r):
+            self.values[i] += first - (i - l)
+
+    '''
+    Return segment's minimum value and the value's index
+    '''
+    def get_minimum(self):
+        min_value, min_shift = self.values[0], 0
+
+        for i in xrange(n):
+            if self.values[i] < min_value:
+                min_value = self.values[i]
+                min_shift = i
+
+        return min_value, min_shift
+
+
 n = int(raw_input())
 
-ps = map(int, raw_input().split(' '))
+deviations = OfflineSegment(n)
 
-deviations = [0 for i in xrange(n)]
+ps = map(int, raw_input().split(' '))
 
 for i in xrange(n):
     # Add abs(ps[i] - ((i + shift) % n + 1)) to each deviations[shift] with absolutes and modulos opened up
 
     if ps[i] >= i + 1:
-        for shift in xrange(0, ps[i] - i):
-            deviations[shift] += ps[i] - (i + shift + 1)
+        deviations.add_decreasing(0, ps[i] - i, ps[i] - (i + 1))
 
-        for shift in xrange(ps[i] - i, n - i):
-            deviations[shift] -= ps[i] - (i + shift + 1)
+        deviations.subtract_increasing(ps[i] - i, n - i, ps[i] - (i + ps[i] - i + 1))
 
-        for shift in xrange(n - i, n):
-            deviations[shift] += ps[i] - (i + shift - n + 1)
+        deviations.add_decreasing(n - i, n, ps[i] - (i + n - i - n + 1))
     else:
-        for shift in xrange(0, n - i):
-            deviations[shift] -= ps[i] - (i + shift + 1)
+        deviations.subtract_increasing(0, n - i, ps[i] - (i + 1))
 
-        for shift in xrange(n - i, n - i + ps[i]):
-            deviations[shift] += ps[i] - (i + shift - n + 1)
+        deviations.add_decreasing(n - i, n - i + ps[i], ps[i] - (i + n - i - n + 1))
 
-        for shift in xrange(n - i + ps[i], n):
-            deviations[shift] -= ps[i] - (i + shift - n + 1)
+        deviations.subtract_increasing(n - i + ps[i], n, ps[i] - (i + n - i + ps[i] - n + 1))
 
-min_deviation, min_shift = deviations[0], 0
-
-for i in xrange(n):
-    if deviations[i] < min_deviation:
-        min_deviation = deviations[i]
-        min_shift = i
+min_deviation, min_shift = deviations.get_minimum()
 
 print min_deviation, min_shift
