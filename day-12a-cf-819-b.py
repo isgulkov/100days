8b158b96b73543802771368ca95d206128ac0ca1
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-26 03:40:17 +0300

    Implement O(n^2) naive solution

--- day-12a-cf-819-b.py
+++ day-12a-cf-819-b.py
@@ -3,5 +3,17 @@ n = int(raw_input())
 
 ps = map(int, raw_input().split(' '))
 
-print ps
+deviations = [0 for i in xrange(n)]
 
+for i in xrange(n):
+    for j in xrange(n):
+        deviations[i] += abs(ps[(j - i) % n] - (j + 1))
+
+min_deviation, min_shift = deviations[0], 0
+
+for i in xrange(n):
+    if deviations[i] < min_deviation:
+        min_deviation = deviations[i]
+        min_shift = i
+
+print min_deviation, min_shift
