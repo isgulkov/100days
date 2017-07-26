8d513b1dd260189c418d72f4c6430194c1ccff8b
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-27 00:18:35 +0300

    Add input template

--- day-12a-cf-819-b.c
+++ day-12a-cf-819-b.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+    int n;
 
+    scanf("%d", &n);
+
+    int* perm = malloc(sizeof(int) * n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", perm + i);
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", perm[i]);
+    }
 
     return 0;
 }
