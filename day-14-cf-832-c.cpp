59ebb70047e194c03d4ce0ee2f150f95b8d46088
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-29 04:55:57 +0300

    Fill reachability arrays in 1000000 steps

--- day-14-cf-832-c.cpp
+++ day-14-cf-832-c.cpp
@@ -19,6 +19,8 @@ class edge_reachability_checker
     std::vector<person> people_facing_left;
     std::vector<person> people_facing_right;
 
+    bool people_sorted = false;
+
 public:
     edge_reachability_checker(int ray_speed) : ray_speed(ray_speed) { }
 
@@ -29,57 +31,87 @@ public:
 
     void add_person_facing_right(int position, int speed)
     {
-        people_facing_right.push_back(person(position, speed));
+        people_facing_right.push_back(person(1000 * 1000 - position, speed));
     }
 
 private:
-    bool left_edge_reachable(person& p, long double time, int bomb_position) {
+    bool edge_reachable(person& p, long double time, int bomb_position) {
+        /**
+         * Determine if person p will reach position 0 in time `time`
+         *
+         * (assuming bomb_position >= p.position)
+         */
+
+        /**
+         * Time that the person will travel at regular speed
+         */
         long double t_regular = (bomb_position - p.position) / (long double)(ray_speed - p.speed);
-        long double d_regular = p.speed * t_regular;
 
-        if(d_regular < 0 || d_regular > p.position) {
-            return p.position / (long double)p.speed <= time;
-        }
+        /**
+         * Distance that the person will cover at regular speed
+         */
+        long double d_regular = t_regular * p.speed;
 
-        long double t_accelerated = t_regular + (p.position - d_regular) / (p.speed + ray_speed);
+        /**
+         * Total distance that the person will travel
+         */
+        long double d_total = d_regular + (p.speed + ray_speed) * (time - t_regular);
 
-        return t_accelerated <= time;
+        return d_total >= p.position;
     };
 
-    bool right_edge_reachable(person& p, long double time, int bomb_position) {
-        long double t_regular = (p.position - bomb_position) / (long double)(ray_speed - p.speed);
-        long double d_regular = p.speed * t_regular;
-
-        if(d_regular < 0 || d_regular > 1000 * 1000 - p.position) {
-            return (1000 * 1000 - p.position) / (long double)p.speed <= time;
+    void populate_reachable(std::vector<person>& people, long double time, std::vector<bool>& reachable)
+    {
+        for(person& p : people) {
+            if(p.position <= time * p.speed) {
+                std::fill(reachable.begin(), reachable.end(), true);
+                return;
+            }
         }
 
-        long double t_total = t_regular + (1000 * 1000 - p.position - d_regular) / (p.speed + ray_speed);
+        auto it_person = people.begin();
 
-        return t_total <= time;
-    };
+        for(int p_bomb = 0; p_bomb <= 1000 * 1000; p_bomb++) {
+            reachable[p_bomb] = false;
+
+            while(it_person != people.end() && it_person->position <= p_bomb) {
+                if(edge_reachable(*it_person, time, p_bomb)) {
+                    reachable[p_bomb] = true;
+                    break;
+                }
+                else {
+                    it_person++;
+                }
+            }
+        }
+    }
 
 public:
     bool edges_reachable(long double time)
     {
+        if(!people_sorted) {
+            std::sort(people_facing_left.begin(), people_facing_left.end(), [](person& a, person&b) {
+                return a.position < b.position;
+            });
+
+            std::sort(people_facing_right.begin(), people_facing_right.end(), [](person& a, person&b) {
+                return a.position < b.position;
+            });
+
+            people_sorted = true;
+        }
+
+        std::vector<bool> left_edge_reachable(1000 * 1000 + 1);
+
+        populate_reachable(people_facing_left, time, left_edge_reachable);
+
+        std::vector<bool> right_edge_reachable(1000 * 1000 + 1);
+
+        populate_reachable(people_facing_right, time, right_edge_reachable);
+
         for(int p_bomb = 0; p_bomb <= 1000 * 1000; p_bomb++) {
-            bool left_reachable = false;
-
-            for(person& p : people_facing_left) {
-                if(left_edge_reachable(p, time, p_bomb)) {
-                    left_reachable = true;
-                    break;
-                }
-            }
-
-            if(!left_reachable) {
-                continue;
-            }
-
-            for(person& p : people_facing_right) {
-                if(right_edge_reachable(p, time, p_bomb)) {
-                    return true;
-                }
+            if(left_edge_reachable[p_bomb] && right_edge_reachable[1000 * 1000 - p_bomb]) {
+                return true;
             }
         }
 
