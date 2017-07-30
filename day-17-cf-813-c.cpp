269b42d87a6c1a5f67a732785d8e3a75f17bac2f
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-30 07:25:15 +0300

    Hang tree by 0 node

--- day-17-cf-813-c.cpp
+++ day-17-cf-813-c.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 class dogonyalkee_tree
 {
     int num_nodes, bob_node;
 
+    std::vector<std::vector<int>> adj_lists;
+
+    void hang_by(int u)
+    {
+        for(int v : adj_lists[u]) {
+            adj_lists[v].erase(std::remove(adj_lists[v].begin(), adj_lists[v].end(), u));
+
+            hang_by(v);
+        }
+    }
+
 public:
-    dogonyalkee_tree(int num_nodes, int bob_node) : num_nodes(num_nodes), bob_node(bob_node) { }
+    dogonyalkee_tree(int num_nodes, int bob_node) : num_nodes(num_nodes), bob_node(bob_node),
+                                                    adj_lists((size_t)num_nodes) { }
 
     void add_edge(int u, int v)
     {
+        adj_lists[u].push_back(v);
+        adj_lists[v].push_back(u);
+    }
 
+    int get_answer()
+    {
+        hang_by(0);
+
+        return 1337;
     }
 };
 
@@ -19,7 +41,7 @@ int main()
 
     std::cin >> num_nodes >> bob_node;
 
-    dogonyalkee_tree t(num_nodes, bob_node);
+    dogonyalkee_tree t(num_nodes, bob_node - 1);
 
     for(int i = 0; i < num_nodes - 1; i++) {
         int u, v;
@@ -28,6 +50,8 @@ int main()
 
         t.add_edge(u - 1, v - 1);
     }
+
+    std::cout << t.get_answer() << std::endl;
 
     return 0;
 }
