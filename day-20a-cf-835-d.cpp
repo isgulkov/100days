32416650ab3cec419f1ea2cf00734dee38cdcda5
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-01 02:52:00 +0300

    Fix several bugs
    * incorrectly using string.compare() return value
    * incorrectly choosing substring's right border
    * incorrectly comparing halves
    * not accounting for k-palindromes as (k-1)-palindromes

--- day-20a-cf-835-d.cpp
+++ day-20a-cf-835-d.cpp
@@ -15,27 +15,29 @@ int main()
      * Declare substrings of length one 1-palindromes
      */
 
-    for(int i = 1; i < s.length(); i++) {
-        max_palindrome_degree[i - 1][i] = 1;
+    for(int i = 0; i < s.length(); i++) {
+        max_palindrome_degree[i][i] = 1;
     }
+
+    num_palidromes[0] = (int)s.length();
 
     /**
      * Update max palindrome degree values for substrings in order of increasing length
      */
     for(size_t substr_length = 2; substr_length <= s.length(); substr_length++) {
-        for(size_t l = 0; l < s.length() - substr_length; l++) {
-            size_t r = l + substr_length;
+        for(size_t l = 0; l <= s.length() - substr_length; l++) {
+            size_t r = l + substr_length - 1;
 
             size_t mid = (l + r) / 2;
 
             /**
              * If string length is odd, include middle symbol in neither half
              */
-            size_t left_half_end = (r - l) % 2 == 1 ? mid - 1 : mid;
+            size_t left_half_end = substr_length % 2 == 1 ? mid - 1 : mid;
 
             size_t right_half_start = mid + 1;
 
-            if(!s.compare(l, substr_length / 2, s, mid + 1, substr_length / 2)) {
+            if(s.compare(l, substr_length / 2, s, right_half_start, substr_length / 2) != 0) {
                 /**
                  * If two halves of the substring aren't equal, the substring isn't a k-palindrome
                  */
@@ -48,7 +50,9 @@ int main()
                         max_palindrome_degree[right_half_start][r]
                 );
 
-                num_palidromes[max_palindrome_degree[l][r] - 1] += 1;
+                for(int i = 0; i < max_palindrome_degree[l][r]; i++) {
+                    num_palidromes[i] += 1;
+                }
             }
         }
     }
