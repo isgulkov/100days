433d06d9390a068130a3acb20d47faef067ad258
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-01 14:06:27 +0300

    Distribute leftovers

--- day-19-cf-808-c.py
+++ day-19-cf-808-c.py
@@ -17,8 +17,13 @@ for volume in volumes:
 
 prev_amount = 0
 
+# Assign minimum possible amout to cups of every volume
+
 for volume in sorted(volume_to_amount.keys()):
-    this_amount = max(prev_amount + 1, int(ceil(volume / 2.0)))
+    this_amount = max(
+        prev_amount + 1,
+        int(ceil(volume / 2.0))
+        )
 
     volume_to_amount[volume] = this_amount
     total_amount -= this_amount * volume_to_count[volume]
@@ -28,7 +33,45 @@ for volume in sorted(volume_to_amount.keys()):
 
     prev_amount = this_amount
 
+# Try to distribute any leftovers over the cups
+
+cup_amounts = [(volume_to_amount[volumes[i]], i, ) for i in xrange(n)]
+
+cup_amounts.sort(key=lambda x: volumes[x[1]])
+
+prev_amount = float("+inf")
+
+for i in reversed(range(n)):
+    if total_amount == 0:
+        break
+
+    amount, index = cup_amounts[i]
+
+    # If this cup's volume is less that that of the previous one, this cup's amount must also be less
+    if i != n - 1:
+        next_cup_index = cup_amounts[i + 1][1]
+
+        if volumes[index] < volumes[next_cup_index]:
+            prev_amount -= 1
+
+    addition = max(0,
+        min(
+            total_amount,
+            volumes[index] - amount,
+            prev_amount
+            )
+        )
+
+    cup_amounts[i] = (amount + addition, index, )
+
+    prev_amount = amount + addition
+
+cup_amounts.sort(key=lambda x: x[1])
+
 if total_amount >= 0:
-    print [volume_to_amount[v] for v in  volumes]
+    for amount, _ in cup_amounts:
+        print amount,
+
+    print
 else:
     print -1
