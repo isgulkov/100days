8251b0b4d7225128ae00517e28befbb87e775172
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-01 13:01:19 +0300

    Fill in minimum possible amounts

--- day-19-cf-808-c.py
+++ day-19-cf-808-c.py
@@ -1,9 +1,34 @@
+from math import ceil
 
-n, w = map(int, raw_input().split(' '))
+n, total_amount = map(int, raw_input().split(' '))
 
 volumes = map(int, raw_input().split(' '))
-volumes = [(volumes[i], i, ) for i in xrange(n)]
 
-volumes.sort()
+volume_to_amount = {}
+volume_to_count = {}
 
-print volumes
+for volume in volumes:
+    volume_to_amount[volume] = 0
+
+    if volume in volume_to_count:
+        volume_to_count[volume] += 1
+    else:
+        volume_to_count[volume] = 1
+
+prev_amount = 0
+
+for volume in sorted(volume_to_amount.keys()):
+    this_amount = max(prev_amount + 1, int(ceil(volume / 2.0)))
+
+    volume_to_amount[volume] = this_amount
+    total_amount -= this_amount * volume_to_count[volume]
+
+    if total_amount < 0:
+        break
+
+    prev_amount = this_amount
+
+if total_amount >= 0:
+    print [volume_to_amount[v] for v in  volumes]
+else:
+    print -1
