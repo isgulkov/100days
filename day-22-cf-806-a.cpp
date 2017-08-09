21e449e0ae3949b7ed48c7a3542be36be9474353
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-10 01:27:50 +0300

    Implement a sloppy binary search solution

--- day-22-cf-806-a.cpp
+++ day-22-cf-806-a.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
-int minimum_additions(int a, int b, int p, int q)
+struct fraction
 {
-    return 1;
+    int a, b;
+
+    fraction(int a, int b) : a(a), b(b) { }
+};
+
+int minimum_additions(fraction proj, fraction target)
+{
+    int left = 1;
+    int right = INT32_MAX / std::max(target.a, target.b);
+
+    while(right - left > 1) {
+        int mid = left + (right - left) / 2;
+
+        int num_diff = mid * target.a - proj.a;
+        int denom_diff = mid * target.b - proj.b;
+
+        if(0 <= num_diff && num_diff <= denom_diff) {
+            right = mid;
+        }
+        else {
+            left = mid;
+        }
+    }
+
+    if(right * target.a - proj.a > right * target.b - proj.b) {
+        return -1;
+    }
+    else {
+        return right * target.b - proj.b;
+    }
 }
 
 int main()
@@ -12,10 +41,13 @@ int main()
     std::cin >> t;
 
     while(t--) {
-        int a, b, p, q;
+        int x, y, p, q;
 
-        std::cin >> a >> b >> p >> q;
+        std::cin >> x >> y >> p >> q;
 
-        std::cout << minimum_additions(a, b, p, q) << std::endl;
+        fraction xy(x, y);
+        fraction pq(p, q);
+
+        std::cout << minimum_additions(xy, pq) << std::endl;
     }
 }
