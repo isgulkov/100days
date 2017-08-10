f95f4250d86d1cded6bf24eece0112da7d879264
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-10 18:10:11 +0300

    Implement naive solution with O(n^2) pair finding

--- day-23-cf-799-c.py
+++ day-23-cf-799-c.py
@@ -1,4 +1,17 @@
 
+def best_pair_score(fountains, max_cost):
+    best_score = 0
+
+    for i in xrange(len(fountains)):
+        for j in xrange(i + 1, len(fountains)):
+            a = fountains[i]
+            b = fountains[j]
+
+            if a[1] + b[1] <= max_cost and a[0] + b[0] > best_score:
+                best_score = a[0] + b[0]
+
+    return best_score
+
 num_fountains, coins, gems = map(int, raw_input().split(' '))
 
 coin_fountains = []
@@ -14,5 +27,16 @@ for i in xrange(num_fountains):
     elif currency == 'D':
         gem_fountains.append(fountain)
 
-print coin_fountains
-print gem_fountains
+affordable_coin_fountains = filter(lambda f: f[1] <= coins, coin_fountains)
+affordable_gem_fountains = filter(lambda f: f[1] <= gems, gem_fountains)
+
+if len(affordable_coin_fountains) == 0 or len(affordable_gem_fountains) == 0:
+    best_two_currencies_score = 0
+else:
+    best_two_currencies_score = max(affordable_coin_fountains)[0] + max(affordable_gem_fountains)[0]
+
+print max(
+    best_pair_score(affordable_coin_fountains, coins),
+    best_pair_score(affordable_gem_fountains, gems),
+    best_two_currencies_score
+    )
