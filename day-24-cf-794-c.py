0947ebe8b1883d3cc65a431b0a6538dc06ef1b31
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 01:08:11 +0300

    Use deque instead of list for O(1) popping

--- day-24-cf-794-c.py
+++ day-24-cf-794-c.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 player_one = list(raw_input())
 player_two = list(raw_input())
@@ -7,8 +8,8 @@ player_two.sort(key=lambda c: -ord(c))
 
 result = ['?' for c in player_one]
 
-player_one = player_one[:len(result) / 2 + len(result) % 2]
-player_two = player_two[:len(result) / 2]
+player_one = deque(player_one[:len(result) / 2 + len(result) % 2])
+player_two = deque(player_two[:len(result) / 2])
 
 pos_left, pos_right = 0, len(result) - 1
 
@@ -21,7 +22,7 @@ for i in xrange(len(result)):
     if len(player_one) != 0 and len(player_two) != 0 and player_one[0] < player_two[0]:
         # If it is favourable to put current best letter in front, do so
 
-        result[pos_left] = this_player.pop(0)
+        result[pos_left] = this_player.popleft()
 
         pos_left += 1
     else:
