d376a15f640d11e54375552a0e4dbb96de2d5b19
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 16:19:58 +0300

    Fix the math

--- day-28-cf-736-a.py
+++ day-28-cf-736-a.py
@@ -1,10 +1,20 @@
 
-x = int(raw_input()) - 1
+# Returns the minimum number of players that need to be in the tournament for the champion to play at least `x` games
+def players_for_champ_level(x):
+    if x == 1:
+        return 2
+    elif x == 2:
+        return 3
+    else:
+        # For the champion to get to the level `x` (play `x` games) he needs to be level `x` - 1 and there needs to be
+        # another player of level `x` - 2
+        return players_for_champ_level(x - 1) + players_for_champ_level(x - 2)
 
-n_games = 0
+x = int(raw_input())
 
-while x != 0:
-    x /= 2
-    n_games += 1
+champ_level = 0
 
-print n_games
+while players_for_champ_level(champ_level + 1) <= x:
+    champ_level += 1
+
+print champ_level
