a0ad201745bb7c904dac33c9b957591b824b038d
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 22:34:34 +0300

    Drop symbols in order of increasing frequencies

--- day-31-cf-101-a.py
+++ day-31-cf-101-a.py
@@ -1,7 +1,7 @@
 
 s = raw_input()
 
-max_dropped_symbols = int(raw_input())
+max_dropped_chars = int(raw_input())
 
 symbol_freq = {chr(i): 0 for i in xrange(ord('a'), ord('z'))}
 
@@ -12,5 +12,18 @@ symbol_freqs = [(symbol_freq[c], c, ) for c in symbol_freq if symbol_freq[c] != 
 
 symbol_freqs.sort()
 
-for s in symbol_freqs:
-    print s
+# Drop as many charactes as we're allowed to, starting with the least frequest symbols
+
+num_dropped_chars = 0
+dropped_symbols = set()
+
+for freq, c in symbol_freqs:
+    if num_dropped_chars + freq > max_dropped_chars:
+        break
+
+    num_dropped_chars += freq
+    dropped_symbols.add(c)
+
+print len(symbol_freqs) - len(dropped_symbols)
+
+print ''.join([c for c in s if c not in dropped_symbols])
