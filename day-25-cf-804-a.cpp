7f06c43cfa78ba489d170e9d03e6baaba89fcbcb
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 12:45:42 +0300

    Add input template with stub for union-find

---
+++ day-25-cf-804-a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+class union_find
+{
+    int total_components;
+    int current_components;
+
+public:
+    union_find(int num_components) : total_components(num_components) { }
+
+    bool same_component(int a, int b)
+    {
+
+    }
+
+    void merge_components(int a, int b)
+    {
+
+    }
+
+    int size()
+    {
+        return current_components;
+    }
+};
+
+int main()
+{
+    int n;
+
+    std::cin >> n;
+
+    union_find u(n);
+
+    
+}
