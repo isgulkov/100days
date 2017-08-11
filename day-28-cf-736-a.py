1dcd1f3e7ff9ad115c41303b15ebf5175d2ccdad
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 15:26:17 +0300

    Compute logarithm manually in integer arithmetic

--- day-28-cf-736-a.py
+++ day-28-cf-736-a.py
@@ -1,5 +1,10 @@
-from math import log, ceil
 
-x = int(raw_input())
+x = int(raw_input()) - 1
 
-print int(ceil(log(x, 2)))
+n_games = 0
+
+while x != 0:
+    x /= 2
+    n_games += 1
+
+print n_games
