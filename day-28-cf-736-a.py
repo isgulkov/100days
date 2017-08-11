cc9a891019c74a95f058d4d02b5744354b1d9bb4
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 16:21:35 +0300

    Cache intermediate results to achieve O(n) time

--- day-28-cf-736-a.py
+++ day-28-cf-736-a.py
@@ -1,14 +1,20 @@
 
 # Returns the minimum number of players that need to be in the tournament for the champion to play at least `x` games
-def players_for_champ_level(x):
+def players_for_champ_level(x, cache={}):
     if x == 1:
         return 2
     elif x == 2:
         return 3
+    elif x in cache:
+        return cache[x]
     else:
         # For the champion to get to the level `x` (play `x` games) he needs to be level `x` - 1 and there needs to be
         # another player of level `x` - 2
-        return players_for_champ_level(x - 1) + players_for_champ_level(x - 2)
+        x_result = players_for_champ_level(x - 1, cache) + players_for_champ_level(x - 2, cache)
+
+        cache[x] = x_result
+
+        return x_result
 
 x = int(raw_input())
 
