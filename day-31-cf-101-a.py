be10f987d1c1728afd21accfd47239e021d45724
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 22:31:00 +0300

    Sort symbols by frequency

--- day-31-cf-101-a.py
+++ day-31-cf-101-a.py
@@ -8,4 +8,9 @@ symbol_freq = {chr(i): 0 for i in xrange(ord('a'), ord('z'))}
 for c in s:
     symbol_freq[c] += 1
 
-print symbol_freq
+symbol_freqs = [(symbol_freq[c], c, ) for c in symbol_freq if symbol_freq[c] != 0]
+
+symbol_freqs.sort()
+
+for s in symbol_freqs:
+    print s
