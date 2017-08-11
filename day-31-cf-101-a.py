44f25ddbacc59beea9a3ed378a94abb8abe9fe49
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-11 22:28:54 +0300

    Count symbol frequencies

--- day-31-cf-101-a.py
+++ day-31-cf-101-a.py
@@ -3,4 +3,9 @@ s = raw_input()
 
 max_dropped_symbols = int(raw_input())
 
-print s
+symbol_freq = {chr(i): 0 for i in xrange(ord('a'), ord('z'))}
+
+for c in s:
+    symbol_freq[c] += 1
+
+print symbol_freq
