9d176473436590b1cfdce1df2234634bf545102d
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-14 16:04:32 +0300

    Improve bruteforce by only iterating over 2 indices instead of 3

--- day-33-cf-743-c.cpp
+++ day-33-cf-743-c.cpp
@@ -3,30 +3,34 @@
 bool express_as_sum_of_3_fractions(int n, long long& x, long long& y, long long& z)
 {
     /**
-     * Iterating over sums of the three indices first allows to traverse the cube "slice by slice" starting from the
-     * origin, thus encountering the smaller and thus more likely triples earlier
+     * Iterating over sums of the first two indices first allows to traverse the plane "slice by slice" starting from
+     * the origin, thus encountering the smaller and thus more likely doubles earlier
      */
 
-    for(int sum_indices = 3; sum_indices <= 1000 * 1000 * 1000; sum_indices++) {
-        for(x = 1; x <= sum_indices - 2; x++) {
-            for(y = 1; y <= sum_indices - 1 - x; y++) {
-                if(y == x) {
-                    continue;
-                }
+    for(int sum_indices = 2; sum_indices <= 1000 * 1000 * 1000; sum_indices++) {
+        for(x = 1; x <= sum_indices - 1; x++) {
+            y = sum_indices - x;
 
-                z = sum_indices - x - y;
+            if(x == y) {
+                continue;
+            }
 
-                if(z == x || z == y) {
-                    continue;
-                }
+            /**
+             * Try to solve "2/n = 1/x + 1/y + 1/z" for z.
+             */
 
-                long long num = 2 * x * y * z;
+            long long num = n * x * y;
 
-                long long denom = x * y + x * z + y * z;
+            long long denom = 2 * x * y - n * (x + y);
 
-                if(num % denom == 0 && num / denom == n) {
-                    return true;
-                }
+            if(num % denom != 0) {
+                continue;
+            }
+
+            z = num / denom;
+
+            if(z > 0 && z != x && z != y) {
+                return true;
             }
         }
     }
