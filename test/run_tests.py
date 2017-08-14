35460eb2cc5b8cc8d5af3fca0e5e922d9149d130
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-14 18:29:59 +0300

    Automatically prepend "python " to .py files in run_tests.py

--- test/run_tests.py
+++ test/run_tests.py
@@ -2,20 +2,24 @@ from os import system
 from sys import argv
 from glob import glob
 
-programs = [argv[1], ]
+commands = [argv[1], ]
 
 filenames_start = 2
 
 if argv[2] in ['-a', '--against']:
-	programs.append(argv[3])
+	commands.append(argv[3])
 
 	filenames_start += 2
+
+for i in xrange(len(commands)):
+    if commands[i][-3:] == '.py':
+        commands[i] = "python " + commands[i]
 
 for test_file in argv[filenames_start:]:
 	print test_file, ": "
 
-	for program in programs:
-		if len(programs) > 1:
+	for program in commands:
+		if len(commands) > 1:
 			print "\t", program
 
 		system("%s < %s" % (program, test_file))
