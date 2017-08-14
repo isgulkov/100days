03616f97496121e4f7c6474db80eabdd9afa6491
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-14 03:20:30 +0300

    Implement an O(n) dp solution

--- day-32-cf-706-c.cpp
+++ day-32-cf-706-c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
@@ -19,7 +20,53 @@ int main()
         std::cin >> words[i];
     }
 
-    for(std::string w : words) {
-        std::cout << w << std::endl;
+    std::vector<std::string> reversed_words(words);
+
+    for(std::string& w : reversed_words) {
+        std::reverse(w.begin(), w.end());
+    }
+
+    /**
+     * min_original[i] stores the minimum number of reversals required to achieve non-descending order of first (i + 1)
+     * words with `i`th word not reversed (`INT32_MAX` if not achievable)
+     */
+    std::vector<int> min_original((size_t)num_words);
+
+    /**
+     * Same, but with the `i`th word reversed
+     */
+    std::vector<int> min_reversed((size_t)num_words);
+
+    min_original[0] = 0;
+    min_reversed[0] = 1;
+
+    for(int i = 1; i < num_words; i++) {
+        min_original[i] = INT32_MAX;
+
+        if(words[i] >= words[i - 1]) {
+            min_original[i] = std::min(min_original[i], min_original[i - 1]);
+        }
+
+        if(words[i] >= reversed_words[i - 1]) {
+            min_original[i] = std::min(min_original[i], min_reversed[i - 1]);
+        }
+
+        min_reversed[i] = INT32_MAX;
+
+        if(reversed_words[i] >= words[i - 1] && min_original[i - 1] != INT32_MAX) {
+            min_reversed[i] = std::min(min_reversed[i], min_original[i - 1] + 1);
+        }
+        if(reversed_words[i] >= reversed_words[i - 1] && min_reversed[i - 1] != INT32_MAX) {
+            min_reversed[i] = std::min(min_reversed[i], min_reversed[i - 1] + 1);
+        }
+    }
+
+    int result = std::min(min_original[num_words - 1], min_reversed[num_words - 1]);
+
+    if(result != INT32_MAX) {
+        std::cout << result << std::endl;
+    }
+    else {
+        std::cout << -1 << std::endl;
     }
 }
