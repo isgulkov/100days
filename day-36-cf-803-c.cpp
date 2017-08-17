33049c3f021168fc08f1fd935622c27b9058933d
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-17 20:29:46 +0300

    Avoid overflow by calculating the sum of progression in 80-bit float

--- day-36-cf-803-c.cpp
+++ day-36-cf-803-c.cpp
@@ -10,27 +10,12 @@ int main()
 
     std::cin >> target_sum >> size;
 
-    int64 max_gcd = -1;
-
     /**
      * As the progression must be strictly increasing and all elements must be divisible by `current_gcd`, to achieve a
      * gcd of `current_gcd`, we need at least the following arithmetic progression: g .. n * g, where g is its GCD
      */
 
-    if(size > 1000 * 1000) {
-        /**
-         * With big enough `size` expressions in the following code may oveflow over the 64-bit integer, but maximum
-         * `size` for which a sequence is achievable with `target_sum` at the maximum 10^10 is about 141 000 anyway
-         */
-
-        std::cout << -1 << std::endl;
-
-        return 0;
-    }
-
-    /**
-     * The target sum must be divisible by the GCD, so iterate over all divisors
-     */
+    int64 max_gcd = -1;
 
     for(int64 i = 1; i * i <= target_sum; i++) {
         if(target_sum % i != 0) {
@@ -38,10 +23,14 @@ int main()
         }
 
         /**
-         * For every divisor less than the square root of the number, there is exactly one greater than it
+         * Iterate over all divisors of `target_sum` -- for every divisor less than the square root of the number, there
+         * is exactly one greater than that
          */
         for(int64 current_gcd : { i, target_sum / i}) {
-            int64 current_sum = current_gcd * size * (size + 1) / 2;
+            /**
+             * Calculate the sum of progression in 80-bit float to avoid overflow
+             */
+            long double current_sum = (long double)current_gcd * size * (size + 1) / 2;
 
             if(current_gcd > max_gcd && current_sum <= target_sum) {
                 max_gcd = current_gcd;
