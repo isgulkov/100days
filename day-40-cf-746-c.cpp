845b285e600d312b3f841d68c47c0b5c5179cca6
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-20 02:39:29 +0300

    Add input template

---
+++ day-40-cf-746-c.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int tram_range, igor_origin, igor_destination;
+
+    std::cin >> tram_range >> igor_origin >> igor_destination;
+
+    int tram_pace, igor_pace;
+
+    std::cin >> tram_pace >> igor_pace;
+
+    int tram_origin, tram_direction;
+
+    std::cin >> tram_origin >> tram_direction;
+
+    
+}
