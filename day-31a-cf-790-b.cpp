e36e80fa9f36cbb81a5d679c438ff43f602cf36c
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-19 23:13:02 +0300

    Fix total deficit calculation

--- day-31a-cf-790-b.cpp
+++ day-31a-cf-790-b.cpp
@@ -21,49 +21,65 @@ public:
 
     long long get_sum_of_jump_path_lengths()
     {
+        /**
+         * Length of a pth measured in jumps is equal to L = ceil(l / `jump_distance`), where l is regular length of the
+         * path.
+         *
+         * Express that as (l + def(l, `jump_distance`)) / `jump_distance`, where def(a, b) is smallest non-negative
+         * integer s.t. a + def(a, b) is divisible by b.
+         *
+         * Thus, Sum(L) for all paths is will be equal to: Sum(l) / k + Sum(def(l, `jump_distance`)) / k.
+         */
+
         long long sum_paths = 0LL;
-        long long total_needs = 0LL;
+        long long total_deficit = 0LL;
 
-        subtree_info tree_result = get_remainders_and_count_nodes(0, -1, 0, sum_paths, total_needs);
+        get_remainders_and_count_nodes(0, -1, sum_paths, total_deficit);
 
-        return (sum_paths + total_needs) / jump_distance;
+        return (sum_paths + total_deficit) / jump_distance;
     }
 
 private:
     struct subtree_info
     {
         std::vector<int> root_remainers;
-
         int node_count;
 
         subtree_info(int jump_distance) : root_remainers((size_t)jump_distance, 0) { }
     };
 
-    subtree_info get_remainders_and_count_nodes(int u, int parent, int depth, long long& sum_paths, long long& total_needs)
+    subtree_info get_remainders_and_count_nodes(int u, int parent, long long& sum_paths, long long& total_deficit)
     {
         subtree_info result(jump_distance);
 
         result.node_count = 1;
+
+        result.root_remainers[0] = 1;
 
         for(int v : adj_lists[u]) {
             if(v == parent) {
                 continue;
             }
 
-            subtree_info subtree_result = get_remainders_and_count_nodes(v, u, depth + 1, sum_paths, total_needs);
+            subtree_info subtree_result = get_remainders_and_count_nodes(v, u, sum_paths, total_deficit);
 
             /**
-             * Add to the sum of all path lengths how many paths go through edge "u -> v"
+             * Add to the sum of all path lengths the number of paths that go through edge "u -> v"
              */
             sum_paths += subtree_result.node_count * (num_nodes - subtree_result.node_count);
 
             result.node_count += subtree_result.node_count;
 
+            /**
+             * For every path between a node in the `v` subtree and a node `u` or any node in all previous subtrees,
+             * add to `total_deficit` how much needs to be added to its length for it to become divisible by
+             * `jump_distance`
+             */
             for(int i = 0; i < jump_distance; i++) {
                 for(int j = 0; j < jump_distance; j++) {
-                    int needs = jump_distance - ((i + j - 2 * depth) % jump_distance + jump_distance) % jump_distance;
+                    int deficit = (2 * jump_distance - (i + j + 1)) % jump_distance;
 
-                    total_needs += needs * subtree_result.root_remainers[i] * result.root_remainers[j];
+                    total_deficit += deficit * subtree_result.root_remainers[i] * result.root_remainers[j];
                 }
             }
 
@@ -71,8 +87,6 @@ private:
                 result.root_remainers[(i + 1) % jump_distance] += subtree_result.root_remainers[i];
             }
         }
-
-        result.root_remainers[1 % jump_distance] += 1;
 
         return result;
     }
