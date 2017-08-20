24fecdde6299440cdb2bad20e39d1ba185f4abf9
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-21 01:26:38 +0300

    Extract chracters better than `min_char` from the "b" stack also

--- day-39-cf-797-c.py
+++ day-39-cf-797-c.py
@@ -10,16 +10,24 @@ b_stack = []
 c_stack = []
 
 while len(a_stack) != 0:
-    current_min = min(a_stack)
+    # It is always the best to extract the minimum character from the "a" stack and put it in front of the string
 
-    while len(b_stack) > 0 and b_stack[-1] == current_min:
+    min_char = min(a_stack)
+
+    # If some occurrences of the current `min_char` or better character accidentally got on top of the "b" stack while
+    # the previous `min_char` was extracted, put them on top of the "c" stack not to bury them when extracting the
+    # current `min_char`
+    while len(b_stack) > 0 and b_stack[-1] <= min_char:
         c_stack.append(b_stack.pop())
 
-    while a_stack[-1] != current_min:
+    # Extract the `min_char`
+    while a_stack[-1] != min_char:
         b_stack.append(a_stack.pop())
 
+    # The actual legal transfer would be a -> b -> c
     c_stack.append(a_stack.pop())
 
+# Dump all that's remaining on the "b" stack onto the "c" stack. Nothing else can be done at this point
 while len(b_stack) != 0:
     c_stack.append(b_stack.pop())
 
