d82099cb1f607c1efaa5bfa10243e7983a2f2c10
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-20 03:01:32 +0300

    Calculate walking time

--- day-40-cf-746-c.cpp
+++ day-40-cf-746-c.cpp
@@ -14,5 +14,23 @@ int main()
 
     std::cin >> tram_origin >> tram_direction;
 
-    
+    /**
+     * Time it takes Igor to walk to his destination by foot
+     */
+    int walking_time = std::abs(igor_origin - igor_destination) * igor_pace;
+
+    /**
+     * Time it takes for Igor and the tram to meet and then for the tram to reach Igor's destination
+     */
+    int ride_time;
+
+    {
+        int time_meet_left = 1000;
+
+        int time_meet_right = 1000;
+
+        ride_time = std::min(time_meet_left, time_meet_right);
+    }
+
+    std::cout << std::min(walking_time, ride_time) << std::endl;
 }
