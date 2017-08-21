115f37328a78a364618270d9f65b207714ceb75f
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-22 01:20:17 +0300

    Calculate the two parts of the tram ride separately
    First to Igor's origin, then to Igor's destination, considering only the pace of
    the tram

--- day-40-cf-746-c.cpp
+++ day-40-cf-746-c.cpp
@@ -20,53 +20,51 @@ int main()
     int walking_time = std::abs(igor_origin - igor_destination) * igor_pace;
 
     /**
-     * Time it takes for the tram to get to Igor's destination
+     * Time it takes Igor to ride the tram to his destination
+     *
+     * Suppose Igor will wait for the tram at `igor_origin` -- there is no point to chase the tram because the time it
+     * takes for the tram to reach Igor's destination doesn't depend on it
      */
     int ride_time;
 
-    if(tram_direction == 1 && tram_origin <= igor_origin) {
+    /**
+     * Calculate the time it takes the tram to reach `igor_origin`
+     */
+
+    if(tram_direction == 1 && tram_origin > igor_origin) {
         /**
-         * Tram catches Igor before reaching end of the line
+         * Got to make a round-trip to the end of the line
          */
 
-        ride_time = tram_pace * std::abs(tram_origin - igor_destination);
-
-        if(igor_destination < tram_origin) {
-            /**
-             * Tram has to make a round-trip to the end of the line
-             */
-
-            ride_time += 2 * tram_pace * (tram_range - tram_origin);
-        }
+        ride_time = tram_pace * ((tram_range - tram_origin) + (tram_range - igor_origin));
     }
-    else if(tram_direction == -1 && tram_origin >= igor_origin) {
+    else if(tram_direction == -1 && tram_origin < igor_origin) {
         /**
-         * Tram catches Igor before reaching the beginning of the line
+         * Got to make a round-trip to the beginning of the line
          */
 
-        ride_time = tram_pace * std::abs(tram_origin - igor_destination);
-
-        if(igor_destination > tram_origin) {
-            /**
-             * Tram has to make a round-trip to the beginning of the line
-             */
-
-            ride_time += 2 * tram_pace * tram_origin;
-        }
+        ride_time = tram_pace * (tram_origin + igor_origin);
     }
-    else if(tram_direction == 1 && tram_origin > igor_origin) {
+    else {
         /**
-         * Tram has to make a round-trip to catch Igor
+         * No round-trip required
          */
 
-        ride_time = tram_pace * ((tram_range - tram_origin) + tram_range + igor_destination);
+        ride_time = tram_pace * std::abs(tram_origin - igor_origin);
     }
-    else if(tram_direction == -1 && tram_origin < igor_destination) {
-        /**
-         * same
-         */
 
-        ride_time = tram_pace * (tram_origin + tram_range + (tram_range - igor_destination));
+    /**
+     * Add the time it now takes the tram to reach `igor_destination` from `igor_origin`
+     */
+
+    if(tram_direction == 1 && igor_origin > igor_destination) {
+        ride_time += tram_pace * ((tram_range - igor_origin) + (tram_range - igor_destination));
+    }
+    else if(tram_direction == -1 && igor_origin < igor_destination) {
+        ride_time += tram_pace * (igor_origin + igor_destination);
+    }
+    else {
+        ride_time += tram_pace * std::abs(igor_origin - igor_destination);
     }
 
     std::cout << std::min(walking_time, ride_time) << std::endl;
