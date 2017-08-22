8bea6d99a1661b5a7225cb0e7ef4f32be0fa1af1
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-08-22 07:02:55 +0300

    Implement a simple DFS solution

--- day-41-cf-802-j.cpp
+++ day-41-cf-802-j.cpp
@@ -13,6 +13,36 @@ public:
         adj_lists[u].push_back(std::make_pair(v, cost));
         adj_lists[v].push_back(std::make_pair(u, cost));
     }
+
+private:
+    void max_journey_dfs(int u, int parent, int current_cost, int& max_cost)
+    {
+        if(current_cost > max_cost) {
+            max_cost = current_cost;
+        }
+
+        for(std::pair<int, int> pair : adj_lists[u]) {
+            int v = pair.first;
+
+            if(v == parent) {
+                continue;
+            }
+
+            int edge_cost = pair.second;
+
+            max_journey_dfs(v, u, current_cost + edge_cost, max_cost);
+        }
+    }
+
+public:
+    int get_max_journey_cost(int start)
+    {
+        int max_cost = 0;
+
+        max_journey_dfs(start, -1, 0, max_cost);
+
+        return max_cost;
+    }
 };
 
 int main()
@@ -31,5 +61,5 @@ int main()
         g.add_edge(u, v, cost);
     }
 
-    
+    std::cout << g.get_max_journey_cost(0) << std::endl;
 }
