fa22025427319cd545c91bd93d36cfbc3246805a
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-07 06:53:20 +0300

    Implement naive O(n^2) solution

--- day-49-cf-853-a.cpp
+++ day-49-cf-853-a.cpp
@@ -15,5 +15,69 @@ int main()
         std::cin >> unit_delay_costs[i];
     }
 
+    int64_t total_delay_cost = 0LL;
 
+    /**
+     * We need to minimize the following expression:
+     *   Sum(c_i * (t_i - i)),
+     * where for a flight with scheduled departuare time `i`, `c_i` is the unit cost of delay of the flight and `t_i` is
+     * the actual departure time. Rearrange the expression:
+     *   Sum(c_i * t_i) - Sum(c_i * i),
+     * where the second term is fixed and is calculated below, and the first term can be minimized eagerly.
+     */
+
+    for(int i = 0; i < num_flights; i++) {
+        total_delay_cost -= unit_delay_costs[i] * (i + 1);
+    }
+
+    std::vector<int> departure_time((size_t)num_flights, -1);
+
+    for(int t = initial_delay; t < num_flights + initial_delay; t++) {
+        /**
+         * For each departure time `t` find a flight that hasn't been selected for any previous `t`s and has the highest
+         * unit cost among such.
+         *
+         * Suppose the following:
+         * * flight `i` is selected on step `t1`;
+         * * flight `j` is selected on step `t2`;
+         * * `t1` < `t2`.
+         * It can be shown that by choosing flights eagerly in the proposed way, these two flights will chosen in the
+         * order that yields minimal total delay cost:
+         * * if `j` > `t1`, `i` and `j` can't be swapped;
+         * * if `j` <= `t1`, than `i` and `j` a selection can be made where `i` and `j` are swapped. Though only the
+         *   selection where `i` >= `j` will be chosen and this selection is the one that contributes the smaller amount
+         *   to the total delay cost, as from
+         *     `i` >= `j`
+         *     `t1` < `t2`
+         *   trivially follows that
+         *     `t1` * `i` + `t2` * `j` <= `t1` * `j` + `t2` * `i`.
+         */
+
+        int max_i = -1;
+
+        for(int i = 0; i <= std::min(num_flights - 1, t); i++) {
+            if(departure_time[i] != -1) {
+                /**
+                 * Flight has already been chosen to depart by time `t`
+                 */
+
+                continue;
+            }
+
+            if(max_i == -1 || unit_delay_costs[i] > unit_delay_costs[max_i]) {
+                max_i = i;
+            }
+        }
+
+        departure_time[max_i] = t;
+        total_delay_cost += unit_delay_costs[max_i] * (t + 1);
+    }
+
+    std::cout << total_delay_cost << std::endl;
+
+    for(int t : departure_time) {
+        std::cout << (t + 1) << " ";
+    }
+
+    std::cout << std::endl;
 }
