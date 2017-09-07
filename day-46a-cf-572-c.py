39027c778e66a728104e6203e8ff3b1cab31e211
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-07 08:58:45 +0300

    Implement a slightly more clever O(n^2) solution

--- day-46a-cf-572-c.py
+++ day-46a-cf-572-c.py
@@ -1,16 +1,18 @@
 
-a, b, c, total = map(int, raw_input().split(' '))
+a, b, c, total_increase = map(int, raw_input().split(' '))
 
-result = 0
+num_invalid_triples = 0
 
-for a_increase in xrange(0, total + 1):
-    for b_increase in xrange(0, total - a_increase + 1):
-        for c_increase in xrange(0, total - a_increase - b_increase + 1):
-            new_a, new_b, new_c = a + a_increase, b + b_increase, c + c_increase
+for a, b, c in ((a, b, c), (b, a, c), (c, a, b)):
+    for a_increase in xrange(total_increase + 1):
+        bc_max_increase = min(total_increase - a_increase, a + a_increase - (b + c))
 
-            new_a, new_b, new_c = sorted((new_a, new_b, new_c, ))
+        for bc_increase in xrange(0, bc_max_increase + 1):
+            num_invalid_triples += (bc_increase + 1)
 
-            if new_c < new_a + new_b:
-                result += 1
+# The total number of all possible triples of sum less than or equal `total_increase` equals the number of ways to
+# divide a sequence of `total_increase` elements into four parts, which is:
+#   C(`total_increase` + 3, 3) = (`total_increase` + 3)! / (3! * `total_uncrease`!) =
+num_all_triples = (total_increase + 1) * (total_increase + 2) * (total_increase + 3) / 6
 
-print result
+print num_all_triples - num_invalid_triples
