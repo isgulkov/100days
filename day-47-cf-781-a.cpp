cf782ee59cdb0f76eaab2fc0062810c0cafda50e
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-09 11:51:45 +0300

    Colorize node children based on its color and parent's color

--- day-47-cf-781-a.cpp
+++ day-47-cf-781-a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class tree
 {
@@ -15,6 +16,35 @@ public:
         adj_lists[u].push_back(v);
         adj_lists[v].push_back(u);
     }
+
+    int get_node_colors(std::vector<int>& node_colors)
+    {
+        node_colors.resize((size_t)num_nodes);
+
+        colorize_nodes(0, -1, 0, -1, node_colors);
+
+        return 1 + *std::max_element(node_colors.begin(), node_colors.end());
+    }
+
+private:
+    void colorize_nodes(int u, int parent, int u_color, int parent_color, std::vector<int>& node_colors)
+    {
+        node_colors[u] = u_color;
+
+        int other_color = 0;
+
+        for(int v : adj_lists[u]) {
+            if(v == parent) {
+                continue;
+            }
+
+            while(other_color == u_color || other_color == parent_color) {
+                other_color += 1;
+            }
+
+            colorize_nodes(v, u, other_color++, u_color, node_colors);
+        }
+    }
 };
 
 int main()
@@ -25,7 +55,7 @@ int main()
 
     tree t(num_nodes);
 
-    for(int i = 0; i < num_nodes; i++) {
+    for(int i = 0; i < num_nodes - 1; i++) {
         int u, v;
 
         std::cin >> u >> v;
@@ -33,5 +63,13 @@ int main()
         t.add_edge(u - 1, v - 1);
     }
 
+    std::vector<int> node_colors;
 
+    std::cout << t.get_node_colors(node_colors) << std::endl;
+
+    for(int c : node_colors) {
+        std::cout << (c + 1) << " ";
+    }
+
+    std::cout << std::endl;
 }
