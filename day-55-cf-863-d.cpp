dd20795b39e8b26a490c2978c27d12d1e5d5daf8
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-23 03:50:22 +0300

    Rewrite in C++

--- day-55-cf-863-d.cpp
+++ day-55-cf-863-d.cpp
@@ -3,10 +3,7 @@
 
 struct request_t
 {
-    enum request_type { right_shift, reverse };
-
-    request_type t;
-    int l, r;
+    int type, l, r;
 };
 
 int main()
@@ -30,14 +27,50 @@ int main()
 
         std::cin >> t >> l >> r;
 
-        requests[i] = {t == 1 ? request_t::request_type::right_shift : request_t::request_type::reverse, l, r};
+        requests[i] = {t, l - 1, r - 1};
     }
 
     std::vector<int> positions((size_t)num_positions);
 
     for(int i = 0; i < num_positions; i++) {
         std::cin >> positions[i];
+
+        positions[i] -= 1;
     }
 
+    /*
+     * Instead of performing the sequence of requests on the array elements, play the sequence back in reverse order on
+     * the positions of interest. The resulting positions will be the positions in the initial array from which the
+     * elements at positions of interest would have originated after the sequence would've been applied.
+     *
+     * This is possible because both subarray reverse and subarray shift are bijective operations.
+     */
+    for(auto it_request = requests.rbegin(); it_request != requests.rend(); it_request++) {
+        int l = it_request->l;
+        int r = it_request->r;
 
+        if(it_request->type == 1) {
+            for(int& p : positions) {
+                if(p == l) {
+                    p = r;
+                }
+                else if(l < p && p <= r) {
+                    p -= 1;
+                }
+            }
+        }
+        else if(it_request->type == 2) {
+            for(int& p : positions) {
+                if(l <= p && p <= r) {
+                    p = l + r - p;
+                }
+            }
+        }
+    }
+
+    for(int p : positions) {
+        std::cout << initial_values[p] << " ";
+    }
+
+    std::cout << std::endl;
 }
